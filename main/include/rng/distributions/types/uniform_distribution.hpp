73f4009d038779b4ac75faab2ee70cf6583a096d
EricoMeger <ericomeger9@gmail.com>
2026-04-12 21:27:40 -0300

    fix uniform distribution generating negative values, add check for invalid argument

--- main/include/rng/distributions/types/uniform_distribution.hpp
+++ main/include/rng/distributions/types/uniform_distribution.hpp
@@ -4,12 +4,17 @@
 
 class UniformDistribution : public Distribution {
 public:
-    explicit UniformDistribution(int min, int max) : min(min), max(max) {}
+    explicit UniformDistribution(int min, int max) : min(min), max(max) {
+        if(min > max) {
+            throw std::invalid_argument("max should be greater than min");
+        }
+    }
     // distribuicao uniforme vai ser usada so para gerar os ids, entao o sample
     // retorna semmpre um int mesmo
     // a tipagem double é pra menter consistente com a classe
     double sample(RNG& engine) override {
-        return min + (engine.nextInt() % (max - min + 1));
+        int range = max - min + 1;
+        return min + static_cast<int>(engine.generate() % static_cast<uint64_t>(range));
     }
 
     std::string getName() const override { 
